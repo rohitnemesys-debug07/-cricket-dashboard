c4f7dd17307b65a2d7c2bdb7d8ab764d4cb27ed4
Rohit Kumar <rohit.nemesys@gmail.com>
2026-04-14 01:27:30 +0530

    removed unwanted files

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 
 def get_matches():
@@ -10,19 +9,4 @@ def get_matches():
     }
 
     response = requests.get(url, headers=headers)
-
-=======
-import requests
-
-def get_matches():
-    url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
-
-    headers = {
-        "X-RapidAPI-Key": "0ba2479f42msh9d51ba45e7bb989p1fb66ejsn9cbff56b3d07",
-        "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers)
-
->>>>>>> 995b71d784e51988894b4cec7a1591e9bfa22c04
     return response.json()
--- db.py
+++ db.py
@@ -1,5 +1,5 @@
-
 import sqlite3
+
 def get_connection():
     conn = sqlite3.connect("cricket.db")
-
+    return conn
