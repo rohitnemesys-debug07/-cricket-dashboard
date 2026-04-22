dbaeac124a8bc441d9161da0e2bc76d20d61d589
rohitnemesys-debug07 <rohit.nemesys@gmail.com>
2026-04-23 00:59:24 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,10 +4,13 @@ def get_matches():
     url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
 
     headers = {
-        "X-RapidAPI-Key": "0ba2479f42msh9d51ba45e7bb989p1fb66ejsn9cbff56b3d07",
+        "X-RapidAPI-Key": "YOUR_API_KEY",
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
     }
 
     response = requests.get(url, headers=headers)
-    return response.json()
-# test change
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {}
