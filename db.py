56c0fb7040584860fa1ab0df62a1262b2ffbe910
rohitnemesys-debug07 <rohit.nemesys@gmail.com>
2026-04-23 01:00:53 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,17 @@
-import sqlite3
+import psycopg2
+import streamlit as st
 
 def get_connection():
-    conn = sqlite3.connect("cricket.db")
-    return conn
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="cricket_db",
+            user="postgres",
+            password="1234567",  # ⚠️ replace
+            port="5432"
+        )
+        return conn
+
+    except Exception as e:
+        st.error(f"Database Connection Failed: {e}")
+        return None
